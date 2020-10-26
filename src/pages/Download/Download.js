4e60f5f06f1529fb8bec4ab50f90a840790e9891
Nykolle Malone <malone.nykolle@gmail.com>
2020-10-25 21:52:18 -0300

    cleaned

--- src/pages/Download/Download.js
+++ src/pages/Download/Download.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { InfoSection } from '../../components';
-import { homeObjOne, homeObjThree } from './Data';
 
 function SignUp() {
   return (
     <>
-      <InfoSection {...homeObjOne} />
-      <InfoSection {...homeObjThree} />
+
     </>
   );
 }
--- src/pages/Play/Data.js
+++ src/pages/Play/Data.js
@@ -1,23 +1,9 @@
-export const homeObjOne = {
-  lightBg: true,
-  lightText: false,
-  lightTopLine: false,
-  lightTextDesc: false,
-  topLine: 'View Our Products',
-  headline: 'Shop through our catalog of products',
-  description:
-    'We provide worldwide shipping to all countries. If there are any issues, just issue a refund and we will process your request',
-  buttonLabel: 'Shop Now',
-  imgStart: '',
-
-  alt: 'Credit Card'
-};
 
 export const homeObjTwo = {
   primary: true,
-  lightBg: false,
+  lightBg: true,
   lightTopLine: true,
-  lightText: true,
+  lightText: false,
   lightTextDesc: true,
   
   headline: "Pais, responsáveis, educadores e gestores, juntos no combate à obesidade.",
@@ -27,32 +13,4 @@ export const homeObjTwo = {
   img: require('../../images/nutri2.png'),
   alt: 'Vault',
   start: ''
-};
-
-export const homeObjThree = {
-  lightBg: true,
-  lightText: false,
-  lightTextDesc: false,
-  topLine: 'Easy Setup',
-  headline: 'Super fast and simple onboarding process',
-  description:
-    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
-  buttonLabel: 'Start Now',
-  imgStart: 'start',
-
-  alt: 'Vault'
-};
-
-export const homeObjFour = {
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'DATA ANALYTICS',
-  headline: 'Every transaction is stored on our secure cloud database',
-  description:
-    'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
-  buttonLabel: 'Sign Up Now',
-  imgStart: 'start',
-  
-  alt: 'Vault'
 };
